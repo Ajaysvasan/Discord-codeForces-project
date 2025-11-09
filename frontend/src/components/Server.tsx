c6ce984389a3fb57a8bf5ac976183494e0344746
Ajaysvasan <ajay192006@gmail.com>
2025-11-09 20:57:42 +0530

    fuck

--- frontend/src/components/Server.tsx
+++ frontend/src/components/Server.tsx
@@ -1,14 +1,13 @@
 const Server = ()=>{
     return(
-<<<<<<< HEAD
+
         <nav id="server-container">
             <h1>Hello world</h1>
         </nav>
     )
-=======
-        <h2>Hello world</h2>
+
     );
->>>>>>> b7b6f0fe29023bba34caa306059f94b4b1895cdd
+
 }
 
 export default Server;
