cd40f64b5dcc043337430e9e08160b5b6e415898
Ajaysvasan <ajay192006@gmail.com>
2025-12-01 20:19:22 +0530

    code refactoring done

--- frontend/src/components/Server.tsx
+++ frontend/src/components/Server.tsx
@@ -1,55 +1,23 @@
-import { useState} from "react";
 import "../App.css"
-import Channels from "../components/Channels";
-interface Server {
-  serverName: string;
-  serverComponents: string[];
+import "./styles/server.css";
+
+interface ServerProps{
+  server:string;
+  onSelectedServer:(name:string)=>void;
 }
 
-interface SideBarProps {
-  servers: Server[];
+const Server = ({server , onSelectedServer}:ServerProps)=>{
+  console.log(server);
+const handleClick = () => onSelectedServer(server);
+
+return (
+    <div className="server">
+  <button className="server-btn" onClick={handleClick}>
+    {server}
+  </button>
+  </div>
+);
+
 }
-var selectedServer:string =  "";
-const Server: React.FC<SideBarProps> = ({ servers }) => {
+export default Server;
 
-  const [ServerName, setServerName] = useState("");
-  const handleClick = (server:string)=>{
-    selectedServer = server;
-    setServerName(server);
-    console.log(ServerName);
-    console.log(selectedServer); 
-  }
-
-
-  const getServerChannels = (selectedServer:string , servers:any)=>{
-    const channels = servers.find(server=>server.serverName === selectedServer); 
-    return channels.serverComponents;
-  }
-
-  // console.log(getServerChannels(selectedServer , servers));
-
-   return (
-    <div className = "lists">
-      <nav className="serverList">
-        <h3>Servers list</h3>
-        {
-          servers.map((server) => (
-            <div className="serverName" key={server.serverName}>
-              <button className="server-btn" onClick={() => handleClick(server.serverName)}>{server.serverName}</button>
-            </div>
-          ))
-        }
-        </nav>
-        <div className="channelList">
-        {
-          selectedServer !== "" && (
-            <div className="Channel">
-              <Channels channels = {getServerChannels(selectedServer , servers)}/>            
-            </div>
-          )
-        }
-      </div>
-    </div>
-  )
-}
-export default Server;     
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,22 +1,48 @@
+import {useState} from "react";
 import "../App.css"
 import "./styles/homePage.css";
-import Server from "../components/Server"
+import Server from "../components/Server";
+import Channels from "../components/Channels";
+
+interface ServerInfo{
+  server:string;
+  serverComponents:string [];
+}
+
 const HomePage = ()=>{
     const servers = [
         {serverName:"python" , serverComponents:["chat room" , "about room" , "problem statement"] },
         {serverName:"C++"  , serverComponents:["PS" , "solution"]},
         {serverName:"C" ,  serverComponents:["PS" , "solution"]},
     ];
-    
-    if(servers.length){
-        console.log();
-    }
+
+     const getServerChannels = (selectedServer:string , servers:any)=>{
+        const channels = servers.find(server=>server.serverName === selectedServer); 
+        return channels.serverComponents;
+  }
+    const [ServerName, setServerName] = useState("");
 
     return(
         <div className="home">
-            <div className="side-bar">
-                <Server servers={servers}></Server>
-            </div>
+          <div className="serverList">
+
+            {
+              servers.map(server=>(
+                <Server server= {server.serverName} key={server.serverName}
+            onSelectedServer = {setServerName}></Server>
+              ))
+            }
+          </div>
+          <div className="channelList">
+          {
+            ServerName !== "" && (
+              <div className="Channel">
+                <Channels channels = {getServerChannels(ServerName, servers)}/>            
+              </div>
+            )
+          }
+          </div>
+ 
         </div>
     )
 }
