311c778eb7b54016c763cf4322d32c7075651f5e
Ajaysvasan <ajay192006@gmail.com>
2025-12-03 11:33:19 +0530

    done some things

--- frontend/src/components/TextSpace.tsx
+++ frontend/src/components/TextSpace.tsx
@@ -1,12 +1,28 @@
+import {useState} from "react";
 import "./styles/textspace.css";
 interface TextInfo{
   selectedChannel:string
 }
 const TextSpace = ({selectedChannel}:TextInfo)=>{
+    const [message , setMessage] = useState("");
+    const [inputValue , setInputValue] = useState("");
+    console.log(selectedChannel);
+    const handleKeyDown = (event:React.KeyboardEvent<HTMLInputElement>)=>{
+        if(event.key == "Enter"){
+            event.preventDefault();
+            setMessage(inputValue);
+            setInputValue("");
+        }
+    }
+
   return(
     <div className="text-space">
-      <h1>Text Channel</h1>
-      {selectedChannel}
+        <div className="message-area">
+            {message}
+        </div>
+        <div className="input-box">
+            <input type="text" value={inputValue} onChange={(e)=>setInputValue(e.target.value)} onKeyDown={handleKeyDown} />
+        </div>
     </div>
   );
 }
