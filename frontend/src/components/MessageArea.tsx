8ceff2fb3da9f5b5a7b5204a0c78dec42667ad1a
Ajaysvasan <ajay192006@gmail.com>
2025-12-05 18:45:54 +0530

    decomposed and add the features

--- frontend/src/components/MessageArea.tsx
+++ frontend/src/components/MessageArea.tsx
@@ -1,24 +1,22 @@
 import {useState} from "react";
+import Textbox from "./TextBox";
+import Message from "./Message";
+import "./styles/textspace.css";
 interface SelectedChannelName{
   selectedChannel:string
 }
 const MessageArea = ({selectedChannel}:SelectedChannelName)=>{
     const [message , setMessage] = useState("");
-    const [inputValue , setInputValue] = useState("");
     console.log(selectedChannel);
-    const handleKeyDown = (event:React.KeyboardEvent<HTMLInputElement>)=>{
-        if(event.key == "Enter"){
-            event.preventDefault();
-            setMessage(inputValue);
-            setInputValue("");
-        }
-    }
+
 
   return(
     <div className="text-component">
         <div className="message-area">
+             <Message messageReceived={message} />
         </div>
         <div className="input-box">
+            <Textbox setMessage={setMessage} />
         </div>
     </div>
   );
