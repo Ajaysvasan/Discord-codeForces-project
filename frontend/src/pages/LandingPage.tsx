cd7c83c5cb1a04fa5e96ae2abf53fea02d54e86f
Ajaysvasan <ajay192006@gmail.com>
2026-02-09 18:38:00 +0530

    feat: added some basic things to the landing page

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,8 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 const LandingPage = () => {
+  const navigate = useNavigate();
+  const [isLoginPage, setLoginPage] = useState(false);
+  const [isRegisterPage, setRegisterPage] = useState(false);
+  const handleLoginClick = () => {
+    setLoginPage(true);
+    setRegisterPage(false);
+    if (isLoginPage) {
+      navigate("/login");
+    }
+  };
+
+  const handleRegisterClick = () => {
+    setLoginPage(false);
+    setRegisterPage(true);
+    if (isRegisterPage) {
+      navigate("/register");
+    }
+  };
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <>
+      <div className="nav-bar">
+        <button onClick={() => handleLoginClick()}>login</button>
+        <button onClick={() => handleRegisterClick()}>register</button>
+      </div>
+    </>
   );
 };
 export default LandingPage;
